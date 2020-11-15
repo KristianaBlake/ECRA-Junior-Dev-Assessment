7b1dba2830b19cc02d0ea7f370519414f5ab87f4
Kristiana Blake <kristiana.blake@gmail.com>
2020-11-15 13:44:59 -0600

    added additional code and psuedo code

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
 from flask import Flask 
 
+DEBUG = True
+PORT = 5000
+
+# Initializes an instance the Flask class 
+# this starts the website! 
 app = Flask(__name__)
 
+# default url
 @app.route('/')
 def index():
 	return "Hello, World!"
 
+# Runs the app when the program starts! 
 if __name__ == "__main__":
 	app.run(debug=True)
