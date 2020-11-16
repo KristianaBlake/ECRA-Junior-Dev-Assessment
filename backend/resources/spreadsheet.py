410d42c819e056926a2a3a345a01fd4c82b2c715
Kristiana Blake <kristiana.blake@gmail.com>
2020-11-15 22:56:17 -0600

    accessed keys and values of spreadsheet

--- backend/resources/spreadsheet.py
+++ backend/resources/spreadsheet.py
@@ -13,8 +13,40 @@ credentials = Credentials.from_service_account_file('client_secret.json', scopes
 
 client = gspread.authorize(credentials)
 
-# Find the spreadsheet by name and open first sheet
+# Finds the workbook by my name and opens the first sheet
+# need to use client name in order to open workbook
 sheet =  client.open("Copy of PrivateNCESForDevs").sheet1
 
-list_of_hashes = sheet.get_all_records()
-print(list_of_hashes)
+# prints hashed list of schools
+list_of_schools = sheet.get_all_records()
+
+# iterated over the indeces of list of schools 
+# printed keys and values 
+# for i in range(len(list_of_schools)):
+# 	print(i, list_of_schools[i])
+
+# this print the values not the keys. The Values are listed (A-AD) 
+# for school in list_of_schools:
+# 	for key in school:
+# 		print(school[key])
+
+# this also print the value not the keys 
+# for school in list_of_schools:
+# 	for value in school:
+# 		print(school[value])
+
+# for school in list_of_schools:
+# 	print(school)
+
+# prints keys in list of schools 
+# for school in list_of_schools:
+# 	keys = school.keys()
+# 	print(keys)
+
+# prints values in list of schools
+for school in list_of_schools:
+	values = school.values()
+	print(values)
+
+
+
